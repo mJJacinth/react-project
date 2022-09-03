c46671ed77b2d9e509938855b9c9caba95b8923e
Jemima <89830930+mJJacinth@users.noreply.github.com>
2022-09-03 18:40:35 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Cards from "./components/cards"
+
+function App(){
+  return(
+    <>
+     <Cards/>
+    </>
+  
+  )
+}
+
+
+
+export default App;
---
+++ src/Data/Product_data.js
@@ -0,0 +1,77 @@
+const product_card = [
+    {
+        "id": 1,
+        "product_name" : "Dolce Gabbana",
+        "price" : "$ 250",
+        "thumb" :"./images/1.png",
+    },
+    {
+        "id":2,
+        "product_name" : "Chopard",
+        "price" :"$ 250",
+        "thumb" :"./images/2.png",
+    },
+    {
+        "id":3,
+        "product_name" : "Absolute",
+        "price" : "$ 250",
+        "thumb" :"./images/3.png",
+    },
+    {
+        "id":4,
+        "product_name" : "Passione",
+        "price" : "$ 250",
+        "thumb" :"./images/4.png",
+    },
+    {
+        "id":5,
+        "product_name" : "Bvlgari",
+        "price" : "$ 250",
+        "thumb" :"./images/5.png",
+    },
+    {
+        "id":6,
+        "product_name" : "Carolina Herrera",
+        "price" : "$ 250",
+        "thumb" :"./images/6.png",
+    },
+    {
+        "id":7,
+        "product_name" : "Splendida",
+        "price" : "$ 250",
+        "thumb" :"./images/7.png",
+    },
+    {
+        "id":8,
+        "product_name" : "Guess",
+        "price" : "$ 250",
+        "thumb" :"./images/8.png",
+    },
+    {
+        "id":9,
+        "product_name" : "Calvin Klein",
+        "price" : "$ 250",
+        "thumb" :"./images/9.png",
+    },
+    {
+        "id":10,
+        "product_name" : "Lattafa",
+        "price" : "$ 250",
+        "thumb" :"./images/10.png",
+    },
+    {
+        "id":11,
+        "product_name" : "Pacco Rabanne",
+        "price" :250,
+        "thumb" :"./images/11.png",
+    },
+    {
+        "id":12,
+        "product_name" : "Victoria Secret",
+        "price" : "$ 250",
+        "thumb" :"./images/12.png",  
+    },
+    
+]
+
+export default product_card;
